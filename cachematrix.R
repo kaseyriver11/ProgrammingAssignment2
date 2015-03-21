662e39bbcf869cd10d453dc73d39c0bc55ed0684
kaseyriver11 <kaseyriver11@gmail.com>
2015-03-21 13:30:16 -0400

    I keep failing to commit correctly!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Sometimes we wish to cache potentially time-consuming computations. Today we are going to do this
+## for the inverse of a matrix. 03/21/15
 
-## Write a short comment describing this function
+## makeCacheMatrix will make a special matrix object that cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+## Here we pull the inverse if the matrix has not been changed, if it has, then we recalculate
+## the inverse
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getinverse()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    inverse<-x$get()
+    m<-solve(inverse, ...)
+    x$setinverse(m)
+    m
 }
